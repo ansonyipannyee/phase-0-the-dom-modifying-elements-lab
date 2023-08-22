7dce7d01a1bbab5938cd33f0dd3bd3cc822b12df
Anson Yip Ann Yee <ansonyipannyee@outlook.com>
2023-08-22 13:01:27 +1000

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+const mainElement = document.getElementById("main");
+mainElement.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.setAttribute("id", "victory");
+
+newHeader.textContent = "Ant is the champion";
